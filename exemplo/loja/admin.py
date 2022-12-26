58445e41c0b43788778a3e0f38343e7418dd1098
Rogério Soares <rogerio_si@yahoo.com.br>
2022-12-26 16:54:18 -0300

    Initial commit

---
+++ exemplo/loja/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(cliente)
+admin.site.register(produto)
+admin.site.register(vendas)
---
+++ exemplo/loja/models.py
@@ -0,0 +1,21 @@
+
+
+from django.db import models
+# Create your models here.
+
+class produto(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
+class cliente(models.Model):
+    title = models.CharField(max_length=100)
+    def __str__(self):
+        return self.title
+
+class vendas(models.Model):
+    role_name = models.CharField(max_length=100)
+    produto = models.ForeignKey('produto', on_delete=models.CASCADE, related_name='produto')
+    cliente = models.ForeignKey('cliente', on_delete=models.CASCADE, related_name='cliente')
+    def __str__(self):
+        return self.role_name
